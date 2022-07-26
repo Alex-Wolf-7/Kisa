a405700bb9514f8e6b0773dd3977c452c6435294
Alex Wolf <alexwolf737@gmail.com>
2022-07-25 17:33:42 -0700

    Created new builds for most recent version

--- constants/constants.go
+++ constants/constants.go
@@ -14,7 +14,7 @@ const MAC_SETTINGS_DB_PATH = "%s/Documents/KisaData"
 const WINDOWS_SETTINGS_DB_PATH = "C:/Users/%s/Documents/KisaData"
 
 const LOG_PERIODIC_ON = false
-const LOG_DEBUG_ON = true
-const LOG_INFO_ON = true
-const LOG_ERROR_ON = true
-const LOG_FATAL_ON = true
+const LOG_DEBUG_ON = false
+const LOG_INFO_ON = false
+const LOG_ERROR_ON = false
+const LOG_FATAL_ON = false
